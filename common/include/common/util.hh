797908e7309a7af847832d2d5d6f6292a0bb7b2a
Julien 'Lta' BALLET <contact@lta.io>
2016-02-17 03:10:26 +0100

    common/util.hh

---
+++ common/include/common/util.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+
+#define DEBUG(msg) std::cout << (msg) << std::endl
+
+namespace blocks
+{
+  struct nocopy
+  {
+    nocopy() = default;
+    ~nocopy() = default;
+
+    nocopy(const nocopy &) = delete;
+    nocopy& operator = (const nocopy &) = delete;
+  };
+}

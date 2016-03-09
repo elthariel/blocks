fe34dcef60b754a547c8272c7144ce2b56224c4d
Julien 'Lta' BALLET <contact@lta.io>
2016-03-09 18:07:19 +0100

    commit compiling thing for champii

--- common/src/rabbit.cc
+++ common/src/rabbit.cc
@@ -3,13 +3,14 @@
 
 namespace blocks
 {
-  Rabbit::Rabbit()
+  RabbitServer::RabbitServer()
     : _conn(amqp_new_connection()), _sock(amqp_tcp_socket_new(_conn))
   {
     if (_sock == nullptr) throw RabbitError("Unable to create socket");
   }
 
-  bool Rabbit::connect(std::string, int, std::string, std::string)
+  bool RabbitServer::connect(std::string host, int port,
+                             std::string login, std::string pasword)
   {
     if (amqp_socket_open(_sock, host.c_str(), port))
     {
@@ -17,13 +18,19 @@ namespace blocks
          return false;
     }
 
-    auto error = amqp_login(
-      _conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest");
-    if (error)
-    {
-         std::cout << "RabbitMq: Unable to login" << std::endl;
-         return false;
-    }
+    auto reply = amqp_login(_conn,
+                            "/",        // VHost
+                            0,          // Chan max
+                            1024 * 128, // framesize
+                            0,          // Hearthbeat
+                            AMQP_SASL_METHOD_PLAIN,
+                            "guest", "guest");
 
+    if (reply.reply_type != AMQP_RESPONSE_NORMAL) {
+      std::cout << "RabbitMq: Unable to login" << std::endl;
+      return false;
+    }
+    _connected = true;
+    return true;
   }
 }
